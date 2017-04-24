bd6c45623be1d93c145e11732492522411d44686
garg10may <garg10may@users.noreply.github.com>
2017-04-24 20:33:36 +0530

    Delete wordFreq.py

--- wordFreq.py
+++
@@ -1,2 +0,0 @@
-from cleanWords import cleanWords
-def wordFreq(textFilePath):        f = open(textFilePath)    countDict = {}        for line in f:        words = cleanWords(line)        for word in words:            countDict[word] = countDict.get(word, 0) + 1        return countDict
